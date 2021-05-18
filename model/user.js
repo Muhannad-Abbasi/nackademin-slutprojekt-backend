47775e52ab48d11b3fd17c2380043b0580dd871a
Medine <you@example.com>
2021-05-18 10:16:58 +0200

    model-user

---
+++ model/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    _id: mongoose.Schema.Types.ObjectId,
+    role: 'admin',
+    adress: {
+        street: String,
+        zip: String,
+        city: String,
+    },
+    orderHistory: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order'
+    }]
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
