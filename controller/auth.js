8d1808b281949e7b7fefe59729c13535a14ea14e
tim.varvikko <tim.varvikko59@gmail.com>
2021-05-18 15:03:28 +0200

    routes update

--- controller/auth.js
+++ controller/auth.js
@@ -1,1 +1,2 @@
 const url = `mongodb+srv://Grin:${process.env.MONGO_DB_ATLAS_PASSWORD}@grin.0ubep.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
--- start.js
+++ start.js
@@ -1,11 +1,15 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 
 const register = require('./routes/register');
 const products = require('./routes/products');
 const orders = require('./routes/orders');
 
+const url = require('./controller/auth')
+
 
 app.use(express.urlencoded({ extended: true }));
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'Sinus-Database' });
 
 app.use('/register', register);
 app.use('/products', products);
