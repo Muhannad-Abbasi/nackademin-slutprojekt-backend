81ddc6dea0730dc29db6841a77ab6344c8fc56cc
tim.varvikko <tim.varvikko59@gmail.com>
2021-05-18 15:02:48 +0200

    route update

--- controller/auth.js
+++ controller/auth.js
@@ -1,1 +1,1 @@
-const url = `mongodb+srv://Grin:${MONGO_DB_ATLAS_PASSWORD}@grin.0ubep.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const url = `mongodb+srv://Grin:${process.env.MONGO_DB_ATLAS_PASSWORD}@grin.0ubep.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
--- start.js
+++ start.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const app = require('./app');
+
+const register = require('./routes/register');
+const products = require('./routes/products');
+const orders = require('./routes/orders');
 
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/register', register);
+app.use('/products', products);
+app.use('/orders', orders);
 
 app.listen(process.env.PORT || 5000, () => console.log("It's running birch!"))
